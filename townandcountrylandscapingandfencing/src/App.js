01affa823b930ec6f0b234e1c0d7e4d70648e43d
StuartCreed <stuart.a.creed@gmail.com>
2020-01-21 10:12:09 +0000

    Initial Commit

--- townandcountrylandscapingandfencing/src/App.js
+++ townandcountrylandscapingandfencing/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import NavBar from "./components/NavBar/NavBar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Stuart Creed's Initial Commit
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <>
+          <NavBar />
+          <header className="App-header">
+            <h1>Town and Country Landscaping and Fencing</h1>
+          </header>
+        </>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ townandcountrylandscapingandfencing/src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default NavBar
+
+/*BELOW IS THE NAVBAR OPERATIONS
+
+*/
