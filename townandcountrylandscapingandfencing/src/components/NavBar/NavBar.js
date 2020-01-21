0f27013b5abdbed6e1c964b37be8e257923526f3
StuartCreed <stuart.a.creed@gmail.com>
2020-01-21 13:33:58 +0000

    NavModalOpening
    This allows the Navbar icon to "open"

--- townandcountrylandscapingandfencing/src/components/NavBar/NavBar.js
+++ townandcountrylandscapingandfencing/src/components/NavBar/NavBar.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import { Component } from 'react';
 import './NavBar.css';
+import $ from "jquery";
 
 class NavBar extends Component {
   constructor(props) {
     super(props);
+       this.state = {
+         isNavModalOpen: "No",
+         isThisAMobile: false
+    };
+    this.toggleNavModal = this.toggleNavModal.bind(this);
+  }
+
+  toggleNavModal() {
+    console.log("toggle initiated")
+    this.setState({
+      isNavModalOpen: "Yes"
+    });
   }
 
   render() {
+
+    function NavModal({isNavModalOpen, toggleNavModal}) {
+      console.log("NavModal initiated")
+      console.log("current state", isNavModalOpen)
+      if (isNavModalOpen == "Yes") {
+        console.log("Nav Modal is open")
+        return (
+          <div id="nav-modal-open">
+            <div id="nav-modal-content">
+              <span id="nav-modal-close" onClick={toggleNavModal}>&times;</span>
+              <p>Some text in the Modal..</p>
+            </div>
+          </div>
+        )
+      }
+
+      else {
+        console.log("Nav Modal is closed")
+        return (
+        <div></div>
+        )
+      }
+    }
+
     return(
-      <div></div>
+      <>
+        <button id="myBtn" onClick={this.toggleNavModal}>Open Modal</button>
+        <NavModal isNavModalOpen={this.state.isNavModalOpen} toggleNavModal={this.toggleNavModal} />
+      </>
     )
   }
 }
 
 export default NavBar
-
-/*BELOW IS THE NAVBAR OPERATIONS
-
-*/
