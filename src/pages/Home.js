cc9226b459a6344442bf7b611e327448f60232d3
Stuart Creed <stuart.a.creed@gmail.com>
2020-12-25 23:09:58 +0000

    Styling changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
   },
   cardInnerTextStyling: {
     marginTop: '20px'
-  },
+  }
 
 }))
 
@@ -51,22 +51,21 @@ export default function Home() {
       <Grid container direction="row-reverse" className={classes.sectionStyling} alignItems="center">
 
         {/*WHAT WE DO CARD*/}
-        <Grid item lg={6} xs={12} className={classes.cardOuterStyling} style={{'textAlign':'center'}}>
+        <Grid item lg={6} xs={12} className={classes.cardOuterStyling}>
           <Typography variant="h2" color='primary'>
             What We Do
           </Typography>
           <Typography variant='subtitle1' color='primary' className={classes.cardInnerTextStyling}>
-            <div style={{'padding':'10px'}}>Founded over 25 years ago, we are known for high quality, value for money work.</div>
-            <div style={{'padding':'10px'}}>From small town gardens to large country estates our services include: </div>
-            <div>
-              <div style={{'padding':'10px'}}><b>Paving</b></div>
-              <div style={{'padding':'10px'}}><b>Patios</b></div>
-              <div style={{'padding':'10px'}}><b>Drives</b></div>
-              <div style={{'padding':'10px'}}><b>Fencing</b></div>
-              <div style={{'padding':'10px'}}><b>Gates</b></div>
-              <div style={{'padding':'10px'}}><b>Gardening</b></div>
-            </div>
-            <div style={{'padding':'10px'}}>If you are looking for landscaping experts for Buckinghamshire and Hertfordshire, contact us now, for your NO OBLIGATION FREE QUOTATION. We would be happy to hear from you. </div>
+            <div style={{'padding':'10px'}}>Founded over 25 years ago, we are known for high quality, value for money work. From small town gardens to large country estates our services include: </div>
+            <ul>
+              <li style={{'padding':'10px'}}><b>Paving</b></li>
+              <li style={{'padding':'10px'}}><b>Patios</b></li>
+              <li style={{'padding':'10px'}}><b>Drives</b></li>
+              <li style={{'padding':'10px'}}><b>Fencing</b></li>
+              <li style={{'padding':'10px'}}><b>Gates</b></li>
+              <li style={{'padding':'10px'}}><b>Gardening</b></li>
+            </ul>
+            <div style={{'padding':'10px'}}>If you are looking for landscaping experts for Buckinghamshire and Hertfordshire, contact us now, for your <b style={{'color':'darkred'}}>No Obligation Free Quotation</b>. We would be happy to hear from you. </div>
           </Typography>
         </Grid>
 
@@ -85,20 +84,20 @@ export default function Home() {
 
         {/*CONTACT US CARD*/}
 
-        <Grid item lg={3} xs={12} className={classes.cardOuterStyling} style={{'textAlign':'center'}}>
+        <Grid item lg={6} xs={12} className={classes.cardOuterStyling} style={{'textAlign':'center'}}>
           <Typography variant='h2' color='primary' >
             Contact Us
           </Typography>
           <Typography variant='subtitle1' color='primary' className={classes.cardInnerTextStyling}>
-            <Link href={contactInformation.AndyPhoneNumberlink}><div><b>Andy Creed's Phone Number: </b>{contactInformation.AndyPhoneNumber}</div></Link>
-            <Link href={contactInformation.RichardPhonelink}><div><b>Richard Salewski's Phone Number: </b>{contactInformation.RichardPhoneNumber}</div></Link>
-            <Link href={contactInformation.Emaillink}><div><b>Email: </b>{contactInformation.Email}</div></Link>
+            <Link href={contactInformation.AndyPhoneNumberlink} style={{'textDecoration':'none'}}><div><b>Andy Creed's Phone Number: </b>{contactInformation.AndyPhoneNumber}</div></Link>
+            <Link href={contactInformation.RichardPhonelink} style={{'textDecoration':'none'}}><div><b>Richard Salewski's Phone Number: </b>{contactInformation.RichardPhoneNumber}</div></Link>
+            <Link href={contactInformation.Emaillink} style={{'textDecoration':'none'}}><div><b>Email: </b>{contactInformation.Email}</div></Link>
           </Typography>
         </Grid>
 
-        <Grid item lg={3} xs={12} style={{'textAlign':'center', 'padding':'20px'}}>
-          <IconButton href={contactInformation.FacebookLink}><FacebookIcon edge color="Primary" style={{'fontSize':'150px'}}/></IconButton>
-        </Grid>
+        {/*<Grid item lg={3} xs={12} style={{'textAlign':'center', 'padding':'20px'}}>*/}
+        {/*  <IconButton href={contactInformation.FacebookLink}><FacebookIcon edge color="Primary" style={{'fontSize':'150px'}}/></IconButton>*/}
+        {/*</Grid>*/}
 
         {/*CONTACT US IMAGES*/}
         <Grid item lg={6} xs={12}>
