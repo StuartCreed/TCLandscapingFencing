5353a68b9e4db1b93a20816b0b1bfc5188121ab5
Stuart Creed <stuart.a.creed@gmail.com>
2020-12-27 07:01:08 +0000

    Photo fix attempt

--- src/shared/services.js
+++ src/shared/services.js
@@ -6,7 +6,7 @@ export const SERVICES =
     ServicePageText: "Paving is our speciality. Ability to do all types of paving including block paving and patios.",
     servicePagePhoto: "./Photos/Paving and Patios/1.jpg",
     PorfolioPagePhotosArray: {
-      photosArray: ["./Photos/Paving and Patios/1.jpg","./Photos/Paving and Patios/2.jpg","./Photos/Paving and Patios/3.jpg", "./Photos/Paving and Patios/4.jpg", "./Photos/Paving and Patios/5.jpg", "./Photos/Paving and Patios/6.jpg", "./Photos/Paving and Patios/7.jpg", "./Photos/Paving and Patios/8.jpg", "./Photos/Paving and Patios/9.jpg", "./Photos/Paving and Patios/10.jpg", "./Photos/Paving and Patios/11.jpg","./Photos/Paving and Patios/12.jpg", "./Photos/Paving and Patios/13.jpg"]
+      photosArray: ["/Photos/Paving and Patios/1.jpg","./Photos/Paving and Patios/2.jpg","./Photos/Paving and Patios/3.jpg", "./Photos/Paving and Patios/4.jpg", "./Photos/Paving and Patios/5.jpg", "./Photos/Paving and Patios/6.jpg", "./Photos/Paving and Patios/7.jpg", "./Photos/Paving and Patios/8.jpg", "./Photos/Paving and Patios/9.jpg", "./Photos/Paving and Patios/10.jpg", "./Photos/Paving and Patios/11.jpg","./Photos/Paving and Patios/12.jpg", "./Photos/Paving and Patios/13.jpg"]
     }
   },
   {
@@ -15,7 +15,7 @@ export const SERVICES =
     ServicePageText: "Close board fencing, panel fencing and gates.",
     servicePagePhoto: "./Photos/Fencing and Gates/1.jpg",
     PorfolioPagePhotosArray: {
-      photosArray: ["./Photos/Fencing and Gates/1.jpg","./Photos/Fencing and Gates/2.jpg","./Photos/Fencing and Gates/3.jpg","./Photos/Fencing and Gates/4.jpg", "./Photos/Fencing and Gates/5.jpg", "./Photos/Fencing and Gates/6.jpg", "./Photos/Fencing and Gates/7.jpg"]
+      photosArray: ["/Photos/Fencing and Gates/1.jpg","./Photos/Fencing and Gates/2.jpg","./Photos/Fencing and Gates/3.jpg","./Photos/Fencing and Gates/4.jpg", "./Photos/Fencing and Gates/5.jpg", "./Photos/Fencing and Gates/6.jpg", "./Photos/Fencing and Gates/7.jpg"]
     }
   },
   {
@@ -24,7 +24,7 @@ export const SERVICES =
     ServicePageText: "Designed and paved to a high standard. Includes resin bonded driveways",
     servicePagePhoto: "./Photos/Driveways/1.jpg",
     PorfolioPagePhotosArray: {
-      photosArray: ["./Photos/Driveways/1.jpg","./Photos/Driveways/2.jpg","./Photos/Driveways/3.jpg", "./Photos/Driveways/4.jpg"]
+      photosArray: ["/Photos/Driveways/1.jpg","./Photos/Driveways/2.jpg","./Photos/Driveways/3.jpg", "./Photos/Driveways/4.jpg"]
     },
   },
   {
@@ -33,7 +33,7 @@ export const SERVICES =
     ServicePageText: "Ponds and water feature installation. We also do Bespoke Pergola design.",
     servicePagePhoto: "./Photos/Ponds and Pergolas/1.jpg",
     PorfolioPagePhotosArray: {
-      photosArray: ["./Photos/Ponds and Pergolas/1.jpg","./Photos/Ponds and Pergolas/2.jpg","./Photos/Ponds and Pergolas/3.jpg", "./Photos/Ponds and Pergolas/4.jpg"]
+      photosArray: ["/Photos/Ponds and Pergolas/1.jpg","./Photos/Ponds and Pergolas/2.jpg","./Photos/Ponds and Pergolas/3.jpg", "./Photos/Ponds and Pergolas/4.jpg"]
     }
   },
   {
@@ -42,7 +42,7 @@ export const SERVICES =
     ServicePageText: "We install all types of decking including composite and hardwood. We also provide services for Lawn turfing, levelling and irrigation.",
     servicePagePhoto: "./Photos/Lawns and Decking/1.jpg",
     PorfolioPagePhotosArray: {
-      photosArray: ["./Photos/Lawns and Decking/1.jpg","./Photos/Lawns and Decking/2.jpg","./Photos/Lawns and Decking/3.jpg", "./Photos/Lawns and Decking/4.jpg"]
+      photosArray: ["/Photos/Lawns and Decking/1.jpg","./Photos/Lawns and Decking/2.jpg","./Photos/Lawns and Decking/3.jpg", "./Photos/Lawns and Decking/4.jpg"]
     }
   },
 ]
