1ee344f7ece4f62bcb1f905722859582a478360d
Stuart Creed <45539606+StuartCreed@users.noreply.github.com>
2023-09-11 14:12:39 +0100

    Update contactInformation.js

--- src/shared/contactInformation.js
+++ src/shared/contactInformation.js
@@ -4,7 +4,7 @@ export const contactInformation =
     AndyPhoneNumberlink: 'tel:07815946340',
     RichardPhoneNumber: '07870519614',
     RichardPhonelink: 'tel:07870519614',
-    Email: 'tcland-enquiries@hotmail.co.uk',
-    Emaillink: 'mailto:tcland-enquiries@hotmail.co.uk',
+    Email: 'tcland.enquiries@gmail.com',
+    Emaillink: 'mailto:tcland.enquiries@gmail.com',
     FacebookLink: 'https://www.facebook.com/townandcountrybucks'
   }
